185994a9c6c0e4faf6427796a6ff0036065a8cba
NicoMaker <nicola.marano02@gmail.com>
2025-05-21 09:59:41 +0200

    file on position home

---
+++ Home/2/script.js
@@ -0,0 +1,6 @@
+// Potenziale script per la funzionalità interattiva
+document.addEventListener("DOMContentLoaded", function () {
+    // Simulazione dell'immagine di sfondo nella sezione wave
+    const waveBg = document.querySelector('.background-image');
+    waveBg.style.backgroundImage = "url('/api/placeholder/400/400')";
+});
